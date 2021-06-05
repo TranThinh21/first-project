7e0cb7355aeb1c77ba1673890030b1165a4a52e3
unknown <trongthinhmemory@gmail.com>
2021-06-05 20:35:57 +0700

    first commit

---
+++ Code/guide.js
@@ -0,0 +1,9 @@
+//In a frying pan, toast the ginger and shallots until blackened.
+ //         2. Place beef bones and cheek in pan and cover with the cold water. Bring to boil; skim off any scum.
+ //         3. Reduce heat. Add the ginger, shallots and remaining stock ingredients; simmer gently for 3-4 hours. Do not let it boil rapidly – liquor will turn cloudy and bitter.
+ //         4. Remove beef cheek, then strain stock. Keep hot in a clean pan.
+   //       5. Blanch rice noodles in boiling water; drain.
+   //       6. Slice cheek into slivers.
+    //      7. Arrange noodles in bowls and top with raw and cooked slices of beef.
+   //       8. Pour over the boiling stock; sprinkle with spring onions.
+   //       9. Serve with remaining garnish.//
